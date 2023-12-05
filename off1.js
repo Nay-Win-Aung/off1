8b9b7c5e2b1393b365ac4d6b8464298ffeb8dfa7
unknown <naywinaung2102022@gmail.com>
2023-12-05 16:09:27 +0630

    node

--- off1.js
+++ off1.js
@@ -10,3 +10,9 @@ http.createServer(function (req, res) {
 //off5
 //off6
 //off7
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
